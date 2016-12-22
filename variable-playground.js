75c44b5a0f732fe18897dcf1154c18c1fcbf4535
eddanieles <ed.danieles@yahoo.com>
2016-12-22 16:44:43 -0500

    Added variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,18 @@
+// var person = {
+//   name: "Horatio",
+//   age: 19
+// }
+//
+// function updatePerson (obj) {
+//   obj.age = 24
+// }
+
+var grades = [24, 123, 531];
+
+function updateGrades (grades) {
+  grades.push(432);
+  debugger;
+}
+
+updateGrades(grades);
+console.log(grades);
